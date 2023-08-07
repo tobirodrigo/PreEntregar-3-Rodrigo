ae602df674e2331dcb95acc150f6fc2180f88ccd
tobirodrigo <tobiasrodrigo2020@gmail.com>
2023-08-07 03:34:23 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -1,28 +1,12 @@
-let searchBtn = document.querySelector("#search-btn")
-let searchForm = document.querySelector (".header .search-form")
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.enlaces-menu');
+const barras = document.querySelectorAll('.ham span');
 
-searchBtn.onclick = () =>{
-    searchBtn.classList.toggle("fa-times");
-    searchForm.classList.toggle("active");
-    menuBtn.classList.remove("fa-times");
-    navbar.classList.remove("active");
-}
 
-let menuBtn = document.querySelector("#menu-btn")
-let navbar = document.querySelector (".header .navbar")
 
-menuBtn.onclick = () =>{
-    menuBtn.classList.toggle("fa-times");
-    navbar.classList.toggle("active");
-    searchBtn.classList.remove("fa-times");
-    searchForm.classList.remove("active");
-}
-
-window.onscroll = () =>{
-    searchBtn.classList.remove("fa-times");
-    searchForm.classList.remove("active");
-    menuBtn.classList.remove("fa-times");
-    navbar.classList.remove("active");
-    }
-
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('animado')});
+    ham.classList.toggle('girar');
+});
 
